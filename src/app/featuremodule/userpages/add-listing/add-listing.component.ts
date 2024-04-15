dd3c521e973a8698cc1f4031f11a546dea486d11
aitnacer-nabil <palancia.gor@gmail.com>
2024-04-15 21:04:17 +0100

    Refactor onFileSelected method to improve readability and maintainability

--- src/app/featuremodule/userpages/add-listing/add-listing.component.ts
+++ src/app/featuremodule/userpages/add-listing/add-listing.component.ts
@@ -39,6 +39,7 @@ export class AddListingComponent implements OnInit {
   categoryId: string = '';
   attributeValues: { [key: string]: string } = {};
   public user: User | null;
+  private imageSrc: any;
 
   constructor(
     private categoriesService: CategoriesService,
@@ -124,6 +125,82 @@ export class AddListingComponent implements OnInit {
 
     } );
 
+  }
+
+// Declare a property to hold the list of image files
+  imageFiles: File[] = [];
+
+
+  onFileSelected(event: any) {
+    if (event.target.files && event.target.files.length > 0) {
+      for (let i = 0; i < event.target.files.length; i++) {
+        const file = event.target.files[i];
+        this.imageFiles.push(file);
+        this.readFileAndLoadImage(file);
+      }
+      console.log('Image files: ', this.imageFiles);
+    }
+  }
+
+  private readFileAndLoadImage(file: File) {
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      const img = new Image();
+      img.onload = () => {
+        const resizedImageSrc = this.resizeImage(img);
+        this.createGalleryUploadDiv(resizedImageSrc, file);
+      };
+      img.src = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  private resizeImage(img: HTMLImageElement): string {
+    const canvas = document.createElement('canvas');
+    canvas.width = 228;
+    canvas.height = 190;
+    const ctx = canvas.getContext('2d');
+    ctx?.drawImage(img, 0, 0, canvas.width, canvas.height);
+    return canvas.toDataURL();
+  }
+
+  private createGalleryUploadDiv(resizedImageSrc: string, file: File) {
+    const galleryUploadDiv = document.createElement('div');
+    galleryUploadDiv.className = 'gallery-upload';
+
+    const imgElement = document.createElement('img');
+    imgElement.className = 'img-fluid';
+    imgElement.alt = '';
+    this.displayImage(resizedImageSrc, imgElement);
+
+    const deleteLink = this.createDeleteLink(file);
+
+    galleryUploadDiv.appendChild(imgElement);
+    galleryUploadDiv.appendChild(deleteLink);
+
+    const gallery = document.querySelector('.galleryimg-upload');
+    if (gallery) {
+      gallery.appendChild(galleryUploadDiv);
+    }
+  }
+
+  private createDeleteLink(file: File): HTMLAnchorElement {
+    const deleteLink = document.createElement('a');
+    deleteLink.href = 'javascript:void(0)';
+    deleteLink.className = 'profile-img-del';
+    deleteLink.innerHTML = '<i class="feather-trash-2"></i>';
+    deleteLink.addEventListener('click', () => {
+      deleteLink.parentElement?.remove();
+      const index = this.imageFiles.indexOf(file);
+      if (index > -1) {
+        this.imageFiles.splice(index, 1);
+      }
+      console.log('Image files: ', this.imageFiles);
+    });
+    return deleteLink;
+  }
+  private displayImage(imageSrc: string, imgElement: HTMLImageElement) {
+    imgElement.src = imageSrc;
   }
 }
 
